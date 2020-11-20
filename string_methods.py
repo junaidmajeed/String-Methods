1f7d4eaaa1b5eb94a72c83ef350072c96a2d8d52
junaidmajeed <35539538+junaidmajeed@users.noreply.github.com>
2020-11-20 12:01:38 +0500

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,27 @@
+"""
+Examples to show available string methods in python
+"""
+# Accessing characters in a string
+# index starts from zero
+first = "nyc"[0]
+print(first)
+
+"""
+len()
+lower()
+upper()
+str()
+"""
+
+stri = "This is a Mixed Case"
+print(stri.lower())
+print(stri.upper())
+print(len(stri))
+
+print(stri + str(2))
+
+"""
+Concatenation
+"""
+
+print("Hello"+" "+"World!!")
